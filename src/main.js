3ec5b129a2d988840ffa86e79e184aeaef0c1787
Lino Helms <lino@betaeg.co>
2019-08-13 15:00:16 +0200

    Working sample with properties

--- src/main.js
+++ src/main.js
@@ -3,6 +3,14 @@ import App from './App.vue'
 
 Vue.config.productionTip = false
 
+class TestClass {
+	static prop = 'foo'
+}
+
+if(TestClass.prop === 'foo') {
+	// something
+}
+
 new Vue({
   render: h => h(App),
 }).$mount('#app')
